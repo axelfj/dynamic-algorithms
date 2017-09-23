6256b946b5a073b802eaef25d856e9bd29504f61
Axel Fernández <axl1210@estudiantec.cr>
2017-09-23 15:45:40 -0600

    QuickSort // 100%
    Finished quicksort.

---
+++ Quicksort.py
@@ -0,0 +1,20 @@
+from random import *
+
+def randomArray(x):
+    array = []
+    for i in range(x):
+        array.append(randint(i,x))
+    return array
+        
+def quickSort(array):
+    if array == []:
+        return []
+    else:
+        pivot = array[0]
+        leftValues = [x for x in array[1:] if x < pivot]
+        rightValues = [x for x in array[1:] if x >= pivot]
+        return quickSort(leftValues)+[pivot]+quickSort(rightValues)
+
+array = randomArray(10)
+#print(array)
+print(quickSort(array))
