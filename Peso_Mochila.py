0d6e1349696e08a83c8bfccb5e90f651dc48ada2
Axel Fernández <axl1210@estudiantec.cr>
2017-09-22 20:32:34 -0600

    Backpack problem // 90%
    Missing some corrections in the implementation.

---
+++ Peso_Mochila.py
@@ -0,0 +1,35 @@
+from random import *
+
+x = int(randrange(0,99999))
+
+products = [1,2,3,4,5]
+weight = 11
+matrix = []
+
+def backpack(products, weight):
+    for i in range(len(products)):
+        row = [x * 0 for x in range(weight + 1)]
+        matrix.append(row)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if j >= products[i]:
+                matrix[i][j] = max(matrix[i-1][j], products[i] + matrix[i-1][j-products[i]])
+            else:
+                matrix[i][j] = matrix[i-1][j]
+    return matrix[len(matrix)-1][len(matrix[0])-1]
+
+def selectProducts(products, weight):
+    backpack(products,weight)
+    i = len(matrix)-1
+    j = len(matrix[0])-1
+    selection = []
+    while matrix[i][j] != 0:
+        if (products[i]+sum(selection)) <= weight:
+            selection += [products[i]]
+        
+            i-=1
+        j-=1
+    return selection
+
+#print(backpack(products,weight))
+print(selectProducts(products,weight))
--- Vuelto_Monedas.py
+++ Vuelto_Monedas.py
@@ -2,16 +2,16 @@ from random import *
 
 x = int(randrange(0,15))
 coins = [1,2,3]
-total = x
-matrix = []
+total = 5
+vector = []
 
 def change(coins, total):
     for i in range(total + 1):
-        matrix.append(0)
-    matrix[0] = 1
+        vector.append(0)
+        vector[0] = 1
     for i in range(len(coins)):
         for j in range(coins[i], total + 1):
-            matrix[j] += matrix[j - coins[i]]
-    return matrix
+            vector[j] += vector[j - coins[i]]
+    return vector[total]
 
 print(change(coins, total))
