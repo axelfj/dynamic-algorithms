b7c8deb5fe1acd7cb44594c9cc36e0b88bc0d85b
Axel Fernández <axl1210@estudiantec.cr>
2017-09-24 17:14:10 -0600

    Hanoi Towers 0.2% //
    Added the towers as lists and commented stuff.

--- Hanoi Towers.py
+++ Hanoi Towers.py
@@ -1,1 +1,18 @@
-#this is the file where hanoi towers will be programmed.//
+# In this algorithm we have 3 towers.
+# We're going to represent each tower as a list.
+# A, B and C will be their names.
+A = []
+B = []
+C = []
+
+# In the first tower we have to set the elements we want from the major to the minor.
+
+for i in range(1,4):
+    A.append(i)
+
+A = A[::-1]
+print("We know have the first tower:", A)
+print("")
+
+# We need to send every element from A to C, using the mid tower B.
+# With the condition of not having a major element over a minor.
