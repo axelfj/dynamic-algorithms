90440f63116ae093926cde2915af5a8ae4a48df6
Axel Fernández <axl1210@estudiantec.cr>
2017-10-06 16:00:12 -0600

    Interface instead of GUI because bugs.

--- Interface.py
+++ Interface.py
@@ -20,9 +20,28 @@ def raise_frame(fromFrame,toFrame):
 def exit_frame():
     control.quit()
 
+# First Frames #
 mainFrame = Frame(control)
 algorithmFrame = Frame(control)
 informationFrame = Frame(control)
+
+# Second Frames #
+coinFrame = Frame(control)
+knapsackFrame = Frame(control)
+shortestpathFrame = Frame(control)
+hanoiFrame = Frame(control)
+sortFrame = Frame(control)
+matrixFrame = Frame(control)
+
+# Third Frames #
+floydFrame = Frame(control)
+dijkstraFrame = Frame(control)
+heapFrame = Frame(control)
+quickFrame = Frame(control)
+
+# Fourth Frames #
+heapMaxFrame = Frame(control)
+heapMinFrame = Frame(control)
 
 # MainFrame #
 Label(mainFrame, text='Welcome to our program.').pack()
@@ -40,14 +59,28 @@ Button(informationFrame, text='Back', command=lambda:raise_frame(informationFram
 
 # AlgorithmFrame #
 Label(algorithmFrame, text='Choose your favorite algorithm.').pack()
-Button(algorithmFrame, text='Coin Change.', command=lambda:raise_frame(algorithmFrame,algorithmFrame)).pack(side=TOP, fill=X)
-Button(algorithmFrame, text='Knapsack', command=lambda:raise_frame(algorithmFrame, algorithmFrame)).pack(side=TOP, fill=X)
-Button(algorithmFrame, text='Shortest Path', command=lambda:raise_frame(algorithmFrame,algorithmFrame)).pack(side=TOP, fill=X)
-Button(algorithmFrame, text='Hanoi Towers', command=lambda:raise_frame(algorithmFrame,algorithmFrame)).pack(side=TOP, fill=X)
-Button(algorithmFrame, text='Sort Algorithms', command=lambda:raise_frame(algorithmFrame, algorithmFrame)).pack(side=TOP, fill=X)
-Button(algorithmFrame, text='N-Matrix Product', command=lambda:raise_frame(algorithmFrame, algorithmFrame)).pack(side=TOP, fill=X)
+Button(algorithmFrame, text='Coin Change', command=lambda:raise_frame(algorithmFrame,coinFrame)).pack(side=TOP, fill=X)
+Button(algorithmFrame, text='Knapsack', command=lambda:raise_frame(algorithmFrame, knapsackFrame)).pack(side=TOP, fill=X)
+Button(algorithmFrame, text='Shortest Path', command=lambda:raise_frame(algorithmFrame,shortestpathFrame)).pack(side=TOP, fill=X)
+Button(algorithmFrame, text='Hanoi Towers', command=lambda:raise_frame(algorithmFrame,hanoiFrame)).pack(side=TOP, fill=X)
+Button(algorithmFrame, text='Sort Algorithms', command=lambda:raise_frame(algorithmFrame, sortFrame)).pack(side=TOP, fill=X)
+Button(algorithmFrame, text='N-Matrix Product', command=lambda:raise_frame(algorithmFrame, matrixFrame)).pack(side=TOP, fill=X)
 Button(algorithmFrame, text='Back', command=lambda:raise_frame(algorithmFrame, mainFrame)).pack(side=TOP, fill=X)
-Label(algorithmFrame, text='Thank you for using our program.').pack()
+
+# coinFrame #
+Label(coinFrame, text='This is the coin change algorithm.\n'
+                      'You need to say how much coins you have & the value of each.\n'
+                      '').pack()
+
+# knapsackFrame #
+
+# shortestpathFrame #
+
+# hanoiFrame #
+
+# sortFrame #
+
+# matrixFrame #
 
 raise_frame(mainFrame,mainFrame)
 control.mainloop()
