3d9013b44b67d08cab58462be1ecb0bc96f8452d
Axel Fernández <axl1210@estudiantec.cr>
2017-10-15 11:17:51 -0600

    hanoiFinal //

--- HanoiTowers.py
+++ HanoiTowers.py
@@ -4,24 +4,16 @@
 # Output: initTower, middleTower, finalTower
 #   we put this to prove we sent from the initTower to the finalTower every disk
 
-def raiseTower(height):
+def hanoi(height):
     initTower = [i if i > 0 else i for i in range(height,0,-1)]
     finalTower = []*height
     middleTower = []*height
-    return initTower,finalTower,middleTower
+    def hanoiAux(height, initTower, middleTower, finalTower):
+        if height > 0:
+            hanoiAux(height - 1, initTower, finalTower, middleTower)
+            finalTower.append(initTower.pop())
+            hanoiAux(height - 1, middleTower, initTower, finalTower)
+        return initTower, middleTower, finalTower
+    return hanoiAux(height,initTower,middleTower,finalTower)
 
-def hanoi(height, initTower, middleTower, finalTower):
-    if height > 0:
-        hanoi(height - 1, initTower, finalTower, middleTower)
-        finalTower.append(initTower.pop())
-        hanoi(height - 1, middleTower, initTower, finalTower)
-    return initTower, middleTower, finalTower
-
-
-initTower = [5, 4, 3, 2, 1]
-finalTower = []
-middleTower = []
-
-#print(source, helper, target)
-#print(hanoi(len(initTower), initTower, middleTower, finalTower))
-#print(raiseTower(5))
+print(hanoi(3))
