c74aa725896212c6ef5c994149ba478e5467732f
Axel Fernández <axl1210@estudiantec.cr>
2017-10-14 21:40:39 -0600

    Hanoi Final //

--- HanoiTowers.py
+++ HanoiTowers.py
@@ -1,20 +1,21 @@
-# A, B and C are our towers. We need to formulate the way that you move all the disk from A to C.
-# The condition is, you can't put a bigger disk over a smaller.
+def raiseTower(height):
+    initTower = [i if i > 0 else i for i in range(height,0,-1)]
+    finalTower = []*height
+    middleTower = []*height
+    return initTower,finalTower,middleTower
 
-A = []
-B = []
-C = []
-
-# We receive the height and the three towers.
-def raiseTowers(height, A, B, C):
-    for i in range(height):
-        A.append(i+1)
-        B.append(0)
-        C.append(0)
-    A = A[::-1]
-    print(A,B,C)
-
-#raiseTowers(5,A,B,C)
+def hanoi(height, initTower, middleTower, finalTower):
+    if height > 0:
+        hanoi(height - 1, initTower, finalTower, middleTower)
+        finalTower.append(initTower.pop())
+        hanoi(height - 1, middleTower, initTower, finalTower)
+    return initTower, middleTower, finalTower
 
 
+initTower = [5, 4, 3, 2, 1]
+finalTower = []
+middleTower = []
 
+#print(source, helper, target)
+print(hanoi(len(initTower), initTower, middleTower, finalTower))
+#print(raiseTower(5))
