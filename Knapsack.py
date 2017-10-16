89163e36d57d9a1d819da566e7960ccf0c0b0a67
Axel Fernández <axl1210@estudiantec.cr>
2017-10-15 19:38:29 -0600

    Knapsack without selection

--- Knapsack.py
+++ Knapsack.py
@@ -27,12 +27,11 @@ def knapsack(cantProducts, weight):
 
     # Select Products #
     def selectProducts(products, matrix):
-        print(matrix)
         i = len(matrix) - 1
         j = len(matrix[0]) - 1
         selection = []
         while matrix[i][j] != 0:
-            if matrix[i][j] == matrix[i - 1][j]:
+            if matrix[i][j] != matrix[i - 1][j]:
                 if (products[i] + sum(selection)) <= weight:
                     selection += [products[i]]
                 i -= 1
@@ -41,16 +40,18 @@ def knapsack(cantProducts, weight):
 
     products = arrayProducts(cantProducts)
     values = setValues(products)
-    matrix = [[0]*(weight+1)]*(len(products))
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if j >= products[i]:
-                matrix[i][j] = max(matrix[i-1][j], values[i] + matrix[i-1][j-products[i]])
-                print("the fuck is this\t",matrix[i-1][j], "--\t",matrix[i-1][j-products[i]])
+    matrix = [[0 for _ in range(weight+1)] for _ in range(len(values)+1)]
+
+    for i in range(len(values) + 1):
+        for j in range(weight + 1):
+            if i == 0 or j == 0:
+                matrix[i][j] = 0
+            elif products[i-1] <= j:
+                matrix[i][j] = max(values[i-1] + matrix[i-1][j - products[i-1]], matrix[i-1][j])
             else:
-                matrix[i][j] = matrix[i-1][j]
-    return matrix[len(matrix)-1][len(matrix[0])-1], values, selectProducts(products,matrix)
+                matrix[i][j] = matrix[i - 1][j]
 
 
+    return matrix, matrix[len(values)][weight], values#, selectProducts(products,matrix)
 
-print(knapsack(5,7))
+print(knapsack(3,5))
